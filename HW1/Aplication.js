ae1e38e54cdded105ea48fcebdee44f88f546422
Halil İbrahim GÜNBULAK <67236125+Halil-ibrahim-GUNBULAK@users.noreply.github.com>
2021-10-25 17:30:34 +0300

    "Hw1_All_Changes"

--- HW1/Aplication.js
+++ HW1/Aplication.js
@@ -1,26 +1,22 @@
   
-const L = [0,1, 2, 3, 4, 5, 6, 7, 8, 9]
-const p=[]
+const T = [0,1, 2, 3, 4, 5, 6, 7, 8, 9]
+const q=[0,0,0,0,0,0,0,0,0,0]
 
-for (let λ of L) {
-    p.push(L)
-}
-
-function makeRows(){
+function Row(){
     var sonuc  = ""
     var head =
     `
     <tr>
-        <th></th><th>λ = ${L[0]}</th>
-        <th>λ = ${L[1]}</th>
-        <th>λ = ${L[2]}</th>
-        <th>λ = ${L[3]}</th>
-        <th>λ = ${L[4]}</th>
-        <th>λ = ${L[5]}</th>
-        <th>λ = ${L[6]}</th>
-        <th>λ = ${L[7]}</th>
-        <th>λ = ${L[8]}</th>  
-      
+        <th></th><th>Y = ${T[0]}</th>
+        <th>Y = ${T[1]}</th>
+        <th>Y = ${T[2]}</th>
+        <th>Y = ${T[3]}</th>
+        <th>Y = ${T[4]}</th>
+        <th>Y = ${T[5]}</th>
+        <th>Y = ${T[6]}</th>
+        <th>Y = ${T[7]}</th>
+        <th>Y = ${T[8]}</th>
+        <th>Y = ${T[9]}</th>    
     </tr>
     `
     sonuc = sonuc + head
@@ -28,25 +24,32 @@ function makeRows(){
         var satir = 
         `
         <tr>
-            <th>X = ${i}</th><td>${p[0].toFixed(4)}<span class="tip">C1</span></td>
-            <td>${p[1].toFixed(4)}<span class="tip">C2</span></td>
-            <td>${p[2].toFixed(4)}<span class="tip">C3</span></td>
-            <td>${p[3].toFixed(4)}<span class="tip">C4</span></td>
-            <td>${p[4].toFixed(4)}<span class="tip">C5</span></td>
-            <td>${p[5].toFixed(4)}<span class="tip">C6</span></td>
-            <td>${p[6].toFixed(4)}<span class="tip">C7</span></td>
-            <td>${p[7].toFixed(4)}<span class="tip">C8</span></td>
-            <td>${p[8].toFixed(4)}<span class="tip">C9</span></td>
+            <th>X = ${i}</th>
+            <td>${q[0].toFixed(4)}<span class="tip">Col1</span></td>
+            <td>${q[1].toFixed(4)}<span class="tip">Col2</span></td>
+            <td>${q[2].toFixed(4)}<span class="tip">Col3</span></td>
+            <td>${q[3].toFixed(4)}<span class="tip">Col4</span></td>
+            <td>${q[4].toFixed(4)}<span class="tip">Col5</span></td>
+            <td>${q[5].toFixed(4)}<span class="tip">Col6</span></td>
+            <td>${q[6].toFixed(4)}<span class="tip">Col7</span></td>
+            <td>${q[7].toFixed(4)}<span class="tip">Col8</span></td>
+            <td>${q[8].toFixed(4)}<span class="tip">Col9</span></td>
+            <td>${q[9].toFixed(4)}<span class="tip">Col10</span></td>
         </tr>
 
         `
-        for(let j = 0 ;j< L.length ;j++){
-            p[j] = (Math.exp(p[j] * L[i]))
+        for(let j = 0 ;j<10 ;j++){
+           
+         
+                q[j] =Math.sin((i+1)*j)
+            
+         
         }
         sonuc = sonuc + satir
     }
+    
     document.querySelector("#tablo").innerHTML = sonuc
 }
 
-makeRows()
-document.querySelector("#sample").innerText = makeRows.toString()
+Row()
+document.querySelector("#sample").innerText = Row.toString()
