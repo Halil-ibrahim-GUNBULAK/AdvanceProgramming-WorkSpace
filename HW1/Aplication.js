ef38f0658b61cc6fa7c6449b2ab417cbfce0fba3
Halil İbrahim GÜNBULAK <67236125+Halil-ibrahim-GUNBULAK@users.noreply.github.com>
2021-11-17 15:21:43 +0300

    "Hw1 ve Cw9 Düzenlendi"

--- HW1/Aplication.js
+++ HW1/Aplication.js
@@ -1,5 +1,5 @@
-const T = [0,1, 2, 3, 4, 5, 6, 7, 8, 9]
-const q=[0,0,0,0,0,0,0,0,0,0]
+const T = [0,1, 2, 3, 4, 5, 6, 7, 8, 9,10,11]
+const q=[0,0,0,0,0,0,0,0,0,0,0,0]
 
 function Row(){
     var sonuc  = ""
@@ -8,26 +8,36 @@ function Row(){
 
     `
     <tr> 
-        <th></th><th>Y = ${T[0]}</th> <th>Y = ${T[1]}</th>  <th>Y = ${T[2]}</th>
-        <th>Y = ${T[3]}</th> <th>Y = ${T[4]}</th>  <th>Y = ${T[5]}</th> 
-        <th>Y = ${T[6]}</th> <th>Y = ${T[7]}</th> <th>Y = ${T[8]}</th> <th>Y = ${T[9]}</th>    
+    
+        <th>Y*X</th><th>${T[0]}</th> <th>${T[1]}</th><th>${T[2]}</th>
+        <th>${T[3]}</th> <th>${T[4]}</th><th>${T[5]}</th> 
+        <th>${T[6]}</th> <th>${T[7]}</th> <th>${T[8]}</th> <th>${T[9]}</th>
+        <th>${T[10]}</th> <th>${T[11]}</th>
     </tr>
     `
     sonuc = sonuc + head
-    for (let i=0; i <= 9; i++){
+    for (let i=0; i <=T.length-1; i++){
         var satir = 
         `
 
         <tr>
-            <th>X = ${i}</th>
-            <td>${q[0].toFixed(2)}<span class="tip">Col1</span></td><td>${q[1].toFixed(2)}<span class="tip">Col2</span></td>
-            <td>${q[2].toFixed(2)}<span class="tip">Col3</span></td><td>${q[3].toFixed(2)}<span class="tip">Col4</span></td>
-            <td>${q[4].toFixed(2)}<span class="tip">Col5</span></td><td>${q[5].toFixed(2)}<span class="tip">Col6</span></td>
-            <td>${q[6].toFixed(2)}<span class="tip">Col7</span></td><td>${q[7].toFixed(2)}<span class="tip">Col8</span></td>
-            <td>${q[8].toFixed(2)}<span class="tip">Col9</span></td><td>${q[9].toFixed(2)}<span class="tip">Col10</span></td>
+            <th>${i}</th>
+            <td>${q[0].toFixed(0)}<span class="tip">Col1</span></td>
+            <td>${q[1].toFixed(0)}<span class="tip">Col2</span></td>
+            <td>${q[2].toFixed(0)}<span class="tip">Col3</span></td>
+            <td>${q[3].toFixed(0)}<span class="tip">Col4</span></td>
+            <td>${q[4].toFixed(0)}<span class="tip">Col5</span></td>
+            <td>${q[5].toFixed(0)}<span class="tip">Col6</span></td>
+            <td>${q[6].toFixed(0)}<span class="tip">Col7</span></td>
+            <td>${q[7].toFixed(0)}<span class="tip">Col8</span></td>
+            <td>${q[8].toFixed(0)}<span class="tip">Col9</span></td>
+            <td>${q[9].toFixed(0)}<span class="tip">Col10</span></td>
+            <td>${q[10].toFixed(0)}<span class="tip">Col9</span></td>
+            <td>${q[11].toFixed(0)}<span class="tip">Col10</span></td>
+            
         </tr>
         `
-        for(let j = 0 ;j<10 ;j++){
+        for(let j = 0 ;j<12 ;j++){
                   
                 q[j] =(i+1)*j
                      
